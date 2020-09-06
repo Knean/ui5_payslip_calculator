64128f5a835078ba019a5b7072973050ed30235b
Knean <mario009c@yahoo.com>
2020-09-06 04:02:44 +0100

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "com/myorg/helloWorld/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("com.myorg.helloWorld.controller.MainView", {});
+});
