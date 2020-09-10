452791c4fae5535ae167196e3514be2e32e73dfe
Knean <mario009c@yahoo.com>
2020-09-10 01:40:31 +0100

    i guess it works

--- uimodule/webapp/controller/MainView.controller.js
+++ uimodule/webapp/controller/MainView.controller.js
@@ -1,7 +1,25 @@
 sap.ui.define([
-  "com/myorg/helloWorld/controller/BaseController"
-], function(Controller) {
+  "com/myorg/helloWorld/controller/BaseController",
+  "sap/base/Log",
+  "sap/ui/core/mvc/Controller"
+], function (Controller, log) {
   "use strict";
+  return Controller.extend("com.myorg.helloWorld.controller.MainView", {
 
-  return Controller.extend("com.myorg.helloWorld.controller.MainView", {});
+    onInit: function () {
+      var model = this.getView().getModel("stuff"); //genious name for a data model
+      this.getView().byId("empTable").setModel(model);
+      log.info(model.getProperty("/employees"));
+    },
+
+    onSubmit: function () {
+      var model = this.getView().getModel("stuff");
+      var form = JSON.parse(JSON.stringify(model.getProperty("/form")));//ugly way to make a copy
+      form.mo_salary = form.salary / 12;
+      model.getProperty("/employees").push(form);
+      log.info(model.getProperty("/employees"));
+      log.info(this.getView().byId("empForm"));
+      this.getView().byId("empTable").getModel().updateBindings(true);
+    }
+  });
 });
